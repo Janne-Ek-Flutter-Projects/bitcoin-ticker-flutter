6c4f5c172516ebb631c0abf8ccba6e1f7822e5b9
Jan Ek <janne.ek1409@gmail.com>
2021-02-08 14:40:49 +0100

    MONDAY 080221 em

--- lib/main.dart
+++ lib/main.dart
@@ -7,9 +7,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData.dark().copyWith(
-          primaryColor: Colors.lightBlue,
-          scaffoldBackgroundColor: Colors.white),
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.dark()
+          .copyWith(primaryColor: Colors.lightBlue, scaffoldBackgroundColor: Colors.white),
       home: PriceScreen(),
     );
   }
--- lib/price_screen.dart
+++ lib/price_screen.dart
@@ -6,11 +6,21 @@ class PriceScreen extends StatefulWidget {
 }
 
 class _PriceScreenState extends State<PriceScreen> {
+  String SelectedCurrency = 'USD';
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('🤑 Coin Ticker'),
+        title: Center(
+            child: Text(
+          '🤑 Coin Ticker',
+          style: TextStyle(
+            fontSize: 30.0,
+            fontWeight: FontWeight.bold,
+            color: Colors.white,
+          ),
+        )),
       ),
       body: Column(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
@@ -42,7 +52,29 @@ class _PriceScreenState extends State<PriceScreen> {
             alignment: Alignment.center,
             padding: EdgeInsets.only(bottom: 30.0),
             color: Colors.lightBlue,
-            child: null,
+            child: DropdownButton<String>(
+              value: SelectedCurrency,
+              items: [
+                DropdownMenuItem(
+                  child: Text('USD'),
+                  value: 'USD',
+                ),
+                DropdownMenuItem(
+                  child: Text('EUR'),
+                  value: 'EUR',
+                ),
+                DropdownMenuItem(
+                  child: Text('GBP'),
+                  value: 'GBP',
+                ),
+              ],
+              onChanged: (value) {
+                setState(() {
+                  SelectedCurrency = value;
+                });
+                print(value);
+              },
+            ),
           ),
         ],
       ),
